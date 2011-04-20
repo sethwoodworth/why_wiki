e8beed77559544313c387bd7a1cbb7e5bf9e4ab4
Seth Woodworth <seth@sethish.com>
2011-04-19 23:15:30 -0400

    modify urls routs

--- urls.py
+++ urls.py
@@ -6,11 +6,11 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     # Example:
-    # (r'^why_wiki/', include('why_wiki.foo.urls')),
+    (r'^/', include('why_wiki.wiki_foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
