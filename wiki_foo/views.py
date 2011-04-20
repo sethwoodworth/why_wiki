d9915d995a75115e90c22c8a9e4bc33a04802355
Seth Woodworth <seth@sethish.com>
2011-04-20 02:11:28 -0400

    updated wiki api pulls

--- wiki_foo/views.py
+++ wiki_foo/views.py
@@ -13,9 +13,27 @@ def user_submit(request, username):
     """
     Do api calls and return a page of stats.
     """
-    url_base = "http://en.wikipedia.org/w/api.php?action=query&list=usercontribs&uclimit=5&format=json&ucnamespace=0&ucuser="
-    url = url_base + username
-    resp = urllib.urlopen(url).read()
+    url_base    = "http://en.wikipedia.org/w/api.php?action=query&format=json"
+    edits   = "&list=usercontribs&uclimit=5&ucnamespace=0&ucuser="
+    meta    = "&list=users&usprop=blockinfo|groups|editcount|registration|emailable|gender&ususers="
 
+    url_edits = url_base + edits + username
+    url_meta = url_base + meta + username
+    json = []
 
-    return render_to_response('stats.html', {'resp':resp})
+    resp = urllib.urlopen(url_edits)
+    edits_data = eval( resp.read() )
+    resp.close()
+
+    resp = urllib.urlopen(url_meta)
+    meta_data = eval( resp.read() )
+    resp.close()
+
+    user = {
+        "created": meta_data['query']['users'][0]['registration'],
+        "editcount": meta_data['query']['users'][0]['editcount'],
+        "gender": meta_data['query']['users'][0]['gender'],
+        "userid": meta_data['query']['users'][0]['userid'],
+        }
+
+    return render_to_response('stats.html', {'data': {"user": user, "edits": edits_data}})
