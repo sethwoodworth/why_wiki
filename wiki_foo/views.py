85ad94d101a3f8f1fc9fd1d3520b79c61e406a47
Seth Woodworth <seth@sethish.com>
2011-04-19 23:11:09 -0400

    start with some stock views for our two pages

--- wiki_foo/views.py
+++ wiki_foo/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from django.contrib.auth.models import User
+
+def root(request):
+    """
+    Main page rendering and setup. May contain changing info (recent users) in {} later.
+    """
+    return render_to_response('index.html', {})
+
+def user_submit(request):
+    """
+    Do api calls and return a page of stats.
+    """
+    return render_to_response('stats.html', {})
