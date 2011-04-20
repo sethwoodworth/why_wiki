d2168b446ce51158f7405cefa41b0272f3fe7c0c
Seth Woodworth <seth@sethish.com>
2011-04-19 23:38:00 -0400

    making index url work

--- urls.py
+++ urls.py
@@ -6,11 +6,12 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     # Example:
-    (r'^/', include('why_wiki.wiki_foo.urls')),
+    (r'^$', 'wiki_foo.views.root'),
+    (r'^/(?P<username>[a-z0-9. ]*)$', 'wiki_foo.views.user_submit'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    #(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    (r'^admin/', include(admin.site.urls)),
+    #(r'^admin/', include(admin.site.urls)),
 )
--- wiki_foo/urls.py
+++ wiki_foo/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import *
-from why_wiki.wiki_foo.models import *
+#from why_wiki.wiki_foo.models import *
 
 urlpatterns = patterns('',
-    (r'^$', 'why_wiki.wiki_foo.views.root'),
+    (r'^/', 'why_wiki.wiki_foo.views.root'),
     # check to see what a valid username consists of on wikip
     (r'^/(?P<username>[a-z0-9. ]*)$', 'why_wiki.wiki_foo.views.user_submit'),
     )
